a5cbb0faa607b51adc554bef2e8d93d6e941ef10
Thomas-C-Dang <tcdang123@gmail.com>
2022-06-22 11:50:47 -0700

    Currently working on: Circle Calculator (Still trying to make a code to display the area/circumference of the circle)

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -13,22 +13,26 @@ if __name__ == '__main__':
     
     # Ask the user for the radius in pixels and store it in a variable
     # simpledialog.askinteger()
-    
+    radius = simpledialog.askinteger(title='Make a circle!', prompt="Put in the amount of pixels you want for your circle")
+
     # Make a new turtle
-    
+    Donatello = turtle.Turtle()
+    Donatello.shape('turtle')
     # Have your turtle draw a circle with the correct radius
     # my_turtle.circle()
+    Donatello.circle(radius)
 
     # Call the turtle .penup() method
-
+    Donatello.penup()
     # Move your turtle to a new x,y position using .goto()
-
+    Donatello.goto(39,45)
     # Calculate the area of your circle and store it in a variable
     # Hint, you can use math.pi
-    
+    area = math.pi*radius*radius
     # Write the area of your circle using the turtle .write() method
     # my_turtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
-
+    Donatello.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
     # Hide your turtle
 
     # Call turtle.done()
+
--- _03_if_else/_4_circle_calculator/circle_calculator.py
+++ _03_if_else/_4_circle_calculator/circle_calculator.py
@@ -1,7 +1,22 @@
+import tkinter as tk
+from tkinter import simpledialog, Tk
+import math
+
+window_width = 600
+window_height = 600
 # Write a Python program that asks the user for the radius of a circle.
 # Next, ask the user if they would like to calculate the area or circumference of a circle.
 # If they choose area, display the area of the circle using the radius.
 # Otherwise, display the circumference of the circle using the radius.
 
-#Area = πr^2
-#Circumference = 2πr 
+
+window = Tk()
+window.withdraw()
+
+radius = simpledialog.askinteger(title='Make a circle!', prompt="Put in the amount of pixels you want for your circle")
+answer = simpledialog.askstring(title='What do you want to calculate?', prompt="Do you want to find the Area or the Circumference?")
+Area = math.pi*radius*radius
+Circumference = math.pi*radius*2
+if answer == "Area":
+    canvas.create_circle(75, 200, 400, 450, fill="Area", outline="")
+else:
